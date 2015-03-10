c657031ce6fa82ef4a2858ef96768018d727674c
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-03-10 13:01:07 -0600

    Fixes crack test.

--- tests/enigma_crack_test.rb
+++ tests/enigma_crack_test.rb
@@ -4,13 +4,13 @@ require_relative '../lib/enigma_brute_crack'
 class EnigmaCrackTest < Minitest::Test
 
   def test_it_exists
-    crack = Bruterack.new('sample.txt')
+    crack = Brutecrack.new('sample.txt', 30315)
     assert crack
   end
 
   def test_it_starts_at_key_attempt_00001
-    crack = Bruterack.new('sample.txt')
+    crack = Brutecrack.new('sample.txt', 30315)
     assert '00001', crack.key_s
   end
-  
+
 end
